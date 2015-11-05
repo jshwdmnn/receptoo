77038234c5b2c1bc7d145bfc2d4a16b1d66ab28a
jshwdmnn <s0537813@htw-berlin.de>
2015-11-05 18:07:46 +0100

    generated Comment model and basic tests

---
+++ receptoo/app/models/comment.rb
@@ -0,0 +1,3 @@
+class Comment < ActiveRecord::Base
+  attr_accessor :user, :comment_text
+end
--- receptoo/db/schema.rb
+++ receptoo/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105162539) do
+ActiveRecord::Schema.define(version: 20151105170140) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.datetime "created_at", null: false
