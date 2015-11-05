05302dea78b5bff962a21019eb95b037b574cb70
Stefan Keil <an.stefan.keil@googlemail.com>
2015-11-05 17:01:44 +0100

    Added basic tests for Recipe class

--- receptoo/spec/models/recipe_spec.rb
+++ receptoo/spec/models/recipe_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	rec = Recipe.new
+
+  describe 'A Recipe Model' do
+  	it 'reflects it´s own class' do
+			expect(rec.class).to eql Recipe	
+  	end
+
+  	it 'has fields' do
+  		rec.title 			 = "Oma´s Eintopf"
+			rec.description  = "Eintopf mit Kartoffeln und Würstchen"
+			# rec.ingredients  = Ingredient.new(name: "Tomate", type: IntergredientType.VEGETABLE)
+			rec.category		 = Category.MEDITERREAN
+			rec.duration     = 20
+			rec.comments		 = Comment.new(user: "Klaus", comment_text: "Das war aber lecker.")
+			rec.rating			 = 3
+			rec.creator_name = "Peter Pan"
+  		expect(rec).to have_attributes(title: "Oma´s Eintopf", description: "Eintopf mit Kartoffeln und Würstchen")
+  	end
+
+  end
 end
